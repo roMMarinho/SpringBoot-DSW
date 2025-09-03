9f4587faa6a2511d2727ce5feb6ccddf38c73151
Matheus Marinho <119502301+roMMarinho@users.noreply.github.com>
2025-09-02 21:53:09 -0300

    Atualizando
    aqui eu exclui o que tinha antes do repositório pois estava duplicado e subi este atualizado - 02/09

---
+++ demo-mvc/src/main/java/com/curso/boot/ProjetoUnoApplication.java
@@ -0,0 +1,13 @@
+package com.curso.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoUnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoUnoApplication.class, args);
+	}
+
+}
